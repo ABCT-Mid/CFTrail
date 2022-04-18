895fcd140cf81f71322a83a8f3e8bd71c61f0a61
Chattray <chattray.chea@gmail.com>
2022-04-18 11:27:22 -0700

    rGET and POST handlers created

---
+++ handlePost/index.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const dynamoose = require('dynamoose');
+
+const resultSchema = new dynamoose.Schema({
+  id: String,
+  score: String,
+  username: String
+})
+
+const resultModel = dynamoose.model('Results', resultSchema);
+
+
+exports.handler = async (event) => {
+  const response = { statusCode: null, body: null }
+  
+  const newResult = {
+    id: event.queryStringParameters.id,
+    score: event.queryStringParameters.score,
+    username: event.queryStringParameters.username
+  }
+ 
+  try {
+    let postResult = await resultModel.create(newResult)
+    response.statusCode = 200;
+    response.body = JSON.stringify(postResult)
+  } catch (error) {
+    response.statusCode = 500;
+    response.body = JSON.stringify(new Error('Cannot Post to Results Table'))
+  }
+
+  return response;
+};
