5b29e8eef81232ef8f50d51012b360e0494b4746
Chattray <chattray.chea@gmail.com>
2022-04-22 10:13:59 -0700

    elongated some timers in game play

--- handleGet/index.js
+++ handleGet/index.js
@@ -5,8 +5,14 @@ const resultModel =  require('./result-model');
 
 exports.handler = async (event) => {
   const response = { statusCode: null, body: null };
+
   try {
     let results = await resultModel.scan().exec();
+    response.headers = {
+      "Access-Control-Allow-Headers" : "Content-Type",
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "OPTIONS,POST,GET"
+    }
     response.statusCode = 200;
     response.body = JSON.stringify(results);
   } catch (error) {
--- handlePost/handlePost.test.js
+++ handlePost/handlePost.test.js
@@ -5,7 +5,7 @@ jest.mock('./result-model', () => {
   let resultModel = {
     create: jest.fn(() => resultModel),
   };
-  return resultModel;
+  // return resultModel;
 });
 
 describe('Testing our handlePost lambda function', () => {
@@ -21,21 +21,21 @@ describe('Testing our handlePost lambda function', () => {
   };
   const resultModel = require('./result-model');
 
-  test('Should return a status code of 200 and a resoponse body', async () => {
-    let response =  await handlePost.handler(event);
-    console.log(response);
-    expect(resultModel.create).toHaveBeenCalled();
-    expect(response.statusCode).toEqual(200);
-    expect(response.body).toBeTruthy();
-  });
+  // test('Should return a status code of 200 and a resoponse body', async () => {
+  //   let response =  await handlePost.handler(event);
+  //   console.log(response);
+  //   expect(resultModel.create).toHaveBeenCalled();
+  //   expect(response.statusCode).toEqual(200);
+  //   expect(response.body).toBeTruthy();
+  // });
 
   // To run this test comment out line 8 and 24-30 - then comment out the code below
 
-  // test('Should return a status code of 500 and a resoponse body', async () => {
-  //   let response =  await handlePost.handler(event);
-  //   console.log(response);
-  //   expect(response.statusCode).toEqual(500);
-  //   expect(response.body).toBeTruthy();
-  // });
+  test('Should return a status code of 500 and a resoponse body', async () => {
+    let response =  await handlePost.handler(event);
+    console.log(response);
+    expect(response.statusCode).toEqual(500);
+    expect(response.body).toBeTruthy();
+  });
 
 });
